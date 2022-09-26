63fa16d48262e60beb5a78f642aa2e108c82b4a9
Kavya9294 <kavya.ub9294@gmail.com>
2022-09-26 05:58:57 -0700

    Createed REadMe.txt and added more test cases

--- README.md
+++ README.md
@@ -1,24 +1,37 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Overview of Design Decisions:
+1) Input to the system is via the CLI by 
+	1) passing a filename as an argument,
+	2) passing a filename to STDIN
+2) Read the file contents line by line and process each entrty
+3) Maintain 2 arrays, 1 for valid users and the other for invalid users
+4) Each time an "Add" query is procesed, a user is added as a valid user if the card number is luhn 10 valid with a balance of 0, otherwise, the user is stored with an error value.
+5) Each time a "Charge" query is processed, first the system checks if the user is valid, and then checks if by adding the charge transaction does not increase beyond the limit
+6) Each time a "Credit" query is processed, the system checks if the user is valid and then proceeds to subtract the credit amount from balance.
+7) Since user data is stored in a treeMap, the keys are already sorted and the system writes to the console in the expencted manner.
 
-Things you may want to cover:
+Chose Ruby on Rails:
+Ruby on Rails spins up an entire system that's able to extend the functionality of this system easily. It will also take less time to extend this system to accomodate for more features.
+The future thought of this system is to incorporate the current system with a database which is available out of the box with ruby on rails.
 
-* Ruby version
+Installing required dependencies:
+1) Download code base locally
+2) Requires ruby 2.7.1 and Rails 7.0.4.
+3) After updating ruby and rails, run bundle install
 
-* System dependencies
+How to build, package or compile your code if applicable
+NA
 
-* Configuration
+To run the code:
+cd payPalBank
+ruby ./app/runners/bank_script.rb input.txt || ruby ./app/runners/bank_script.rb < input.txt
+where input.txt has the input credit card transactions details
 
-* Database creation
+To run tests:
+bundle exec rspec ./spec/runners/bank_script_spec.r
 
-* Database initialization
 
-* How to run the test suite
 
-* Services (job queues, cache servers, search engines, etc.)
 
-* Deployment instructions
 
-* ...
--- spec/runners/bank_script_spec.rb
+++ spec/runners/bank_script_spec.rb
@@ -13,51 +13,67 @@ describe BankScript do
 	let(:content_update) { StringIO.new("Add Tom 4111111111111111 $1000\nCharge Tom $7\nCredit Tom $100")}
 	let(:content_charge_limit) { StringIO.new("Add Tom 4111111111111111 $1000\nCharge Tom $500\nCharge Tom $800") }
 	let(:content_error_update) { StringIO.new("Add Tom 1234567890123456 $1000\nCharge Tom $100\nCredit Tom $200")}
+	let(:content_create_users) { StringIO.new("Add Tom 4111111111111111 $1000\nAdd Lisa 5454545454545454 $3000") }
 
   describe "run" do
 		@filename = "file.txt"
 		args =  []
 		args[0] = @filename
+
+		#Testing creation of new user
     it "creates new User with 0 balance" do
 			allow_any_instance_of(Object).to receive(:readlines).and_return(content_create)
       expect{ described_class.run(args) }
       .to output("Tom: $0\n").to_stdout
     end
 
+		#Testing basic charge operation
 		it "creates User with charged balance" do
 			allow_any_instance_of(Object).to receive(:readlines).and_return(content_charge)
       expect{ described_class.run(args) }
       .to output("Tom: $700\n").to_stdout
     end
 
+		#Testing basic credit operation
 		it "creates User with credited balance" do
 			allow_any_instance_of(Object).to receive(:readlines).and_return(content_credit)
       expect{ described_class.run(args) }
       .to output("Tom: $-100\n").to_stdout
     end
 
+		#Testing luhn 10 invalid card scenario
 		it "leads to error due to inavlid card" do
 			allow_any_instance_of(Object).to receive(:readlines).and_return(content_error)
       expect{ described_class.run(args) }
       .to output("Tom: error\n").to_stdout
     end
 
+		#Testing Add, Charge and credit oprations
 		it "Creates user, charges balance and credits balance" do
 			allow_any_instance_of(Object).to receive(:readlines).and_return(content_update)
       expect{ described_class.run(args) }
       .to output("Tom: $-93\n").to_stdout
     end
 
+		#Testing Charge above limit scenario
 		it "creates User and keeps balance within limits by invalidating query" do
 			allow_any_instance_of(Object).to receive(:readlines).and_return(content_charge_limit)
       expect{ described_class.run(args) }
       .to output("Tom: $500\n").to_stdout
     end
 
+		#Testing multiple operations on invalid user
 		it "leads to error due to inavlid card and balance is not updated" do
 			allow_any_instance_of(Object).to receive(:readlines).and_return(content_error_update)
       expect{ described_class.run(args) }
       .to output("Tom: error\n").to_stdout
+    end
+
+		#Testing multiple user creation in alphabetical order
+		it "creates new User with 0 balance" do
+			allow_any_instance_of(Object).to receive(:readlines).and_return(content_create_users)
+      expect{ described_class.run(args) }
+      .to output("Lisa: $0\nTom: $0\n").to_stdout
     end
 
   end
